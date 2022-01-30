649cf54bd7970b313298fd21bf6c94e20c666e05
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-01-30 22:41:41 +0530

    Layout part

---
+++ app/src/main/java/com/gdsc_snu/a10dance/visit_page.kt
@@ -0,0 +1,11 @@
+package com.gdsc_snu.a10dance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class visit_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_page)
+    }
+}
