0f17252b05a5d1d9d32024c7f876b4696b137888
Bhawna chauhan <bhawnabc2002@gmail.com>
2022-02-03 10:29:12 +0530

    completely done with UI of the app

--- app/src/main/java/com/gdsc_snu/a10dance/SplashScreenActivity.kt
+++ app/src/main/java/com/gdsc_snu/a10dance/SplashScreenActivity.kt
@@ -1,25 +1,44 @@
 package com.gdsc_snu.a10dance
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        //this one is for hiding taskBar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
 
-        val backgroundImg: ImageView = findViewById(R.id.iv_logo)
-        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        //variables for animation
+        // Animation topAnim,bottomAnim
+        val backgroundImg : ImageView = findViewById(R.id.iv_logo)
+        val topAnim = AnimationUtils.loadAnimation(this,R.anim.top_navigation)
+        backgroundImg.startAnimation(topAnim)
 
-        backgroundImg.startAnimation(sideAnimation)
+        val textView1 : TextView = findViewById(R.id.textView1)
+        val bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_navigation)
+        textView1.startAnimation(bottomAnim)
+
+        val textView2 : TextView = findViewById(R.id.textView1)
+        val bottomAnim2 = AnimationUtils.loadAnimation(this,R.anim.bottom_navigation)
+        textView2.startAnimation(bottomAnim2)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this,visit_page::class.java))
             finish()
-        }, 4000)
+        },5000)
+
     }
 }
--- app/src/main/java/com/gdsc_snu/a10dance/visit_page.kt
+++ app/src/main/java/com/gdsc_snu/a10dance/visit_page.kt
@@ -1,11 +1,24 @@
 package com.gdsc_snu.a10dance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class visit_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visit_page)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        val btnStartExplore = findViewById<Button>(R.id.btnStart)
+        btnStartExplore.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
